bc73e8d88e7932048447d5e9196163c2c6aa81d0
Daniel Kambich <dkambich@purdue.edu>
2020-10-15 10:27:02 -0400

    Remove extra CSS baselines

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { Box, Typography, Button, Container, Grid, Avatar, CssBaseline, TextField, Link } from '@material-ui/core';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import useStyles from './theme';
+import {
+  Box,
+  Typography,
+  Button,
+  Container,
+  Grid,
+  Avatar,
+  TextField,
+  Link,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import useStyles from "./theme";
 
 function SignIn() {
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
       <div style={useStyles.paper}>
-      <Avatar style={useStyles.avatar}>
+        <Avatar style={useStyles.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5" srtle={{marginTop: 10, marginBottom: 30}}>Sign in</Typography>
+        <Typography
+          component="h1"
+          variant="h5"
+          srtle={{ marginTop: 10, marginBottom: 30 }}
+        >
+          Sign in
+        </Typography>
         <form srtle={useStyles.form} noValidate>
           <TextField
             variant="outlined"
@@ -35,20 +49,28 @@ function SignIn() {
             id="password"
             autoComplete="current-password"
           />
-          <Link href="/" style={{ textDecoration: 'none' }}><Button fullWidth variant="contained" color="primary" style={useStyles.submit} >Sign In</Button></Link>
+          <Link href="/" style={{ textDecoration: "none" }}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              style={useStyles.submit}
+            >
+              Sign In
+            </Button>
+          </Link>
           <Grid container>
             <Grid item>
-            <Link href="/SignUp" variant="body2">{"Don't have an account? Sign Up"}</Link>
+              <Link href="/SignUp" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={8}>
-      </Box>
-    </Container>      
-  );  
+      <Box mt={8}></Box>
+    </Container>
+  );
 }
-
-
 
 export default SignIn;
