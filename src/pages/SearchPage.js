d3d7492258f367d071efbc01a7a90a19538f4712
harachoi <hara77227722@gmail.com>
2020-10-21 23:39:03 +0900

    update search

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,7 +2,20 @@ import React, { useState } from "react";
 import { Select, MenuItem, InputLabel, useTheme, TextField } from "@material-ui/core";
 import SearchBar from "material-ui-search-bar";
 import NavBar from "../components/NavBar";
+import MaterialTable from 'material-table';
+import { SvgIconProps } from '@material-ui/core/SvgIcon'
 
+import Search from '@material-ui/icons/Search'
+import ViewColumn from '@material-ui/icons/ViewColumn'
+import SaveAlt from '@material-ui/icons/SaveAlt'
+import ChevronLeft from '@material-ui/icons/ChevronLeft'
+import ChevronRight from '@material-ui/icons/ChevronRight'
+import FirstPage from '@material-ui/icons/FirstPage'
+import LastPage from '@material-ui/icons/LastPage'
+import Add from '@material-ui/icons/Add'
+import Check from '@material-ui/icons/Check'
+import FilterList from '@material-ui/icons/FilterList'
+import Remove from '@material-ui/icons/Remove'
 
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -30,118 +43,46 @@ function SearchPage() {
   };
   const theme = useTheme();
 
-  const StyledTableCell = withStyles((theme) => ({
-    head: {
-      backgroundColor: theme.palette.primary.contrastText,
-      color: theme.palette.common.black,
-    },
-    body: {
-      fontSize: 14,
-    },
-  }))(TableCell);
-
-  const StyledTableRow = withStyles((theme) => ({
-    root: {
-      '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-      },
-    },
-  }))(TableRow);
-
-  const list = [
-    {
-      'id': 1,
-      'title': '1st Item',
-      'description': 'Description here.'
-    },
-    {
-      'id': 2,
-      'title': '2nd Item',
-      'description': 'Another description here.'
-    },
-    {
-      'id': 3,
-      'title': '3rd Item',
-      'description': 'Third description here.'
-    }
-  ];
-
-  const search = (list) => {
-    return list.filter(list => list.title.toLowerCase().indexOf(q) > -1)
-  }
-
-
   return (
     <dev>
       <NavBar />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          marginTop: 100,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <InputLabel
-          id="demo-controlled-open-select-label"
-          style={{ marginRight: 20 }}
-        >
-          Type
-        </InputLabel>
-
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={select}
-          onChange={handleChange}
-          style={{ marginRight: 20, width: 90 }}
-        >
-          <MenuItem value={0}>All</MenuItem>
-          <MenuItem value={1}>Book</MenuItem>
-          <MenuItem value={2}>Movie</MenuItem>
-          <MenuItem value={3}>Music</MenuItem>
-        </Select>
-        <SearchBar style={{ width: 400 }}>
-            <TextField 
-              type="text"   
-              value={q}        
-              onChange={(e) => setQ(e.title.value)}
-              onRequestSearch={() => console.log("onRequestSearch")}
-            />
-        </SearchBar>
-      </div>
-      <div style={theme.searchpaper}>
-        <Table className={theme.table} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Title</StyledTableCell>
-              <StyledTableCell align="right">Author</StyledTableCell>
-              <StyledTableCell align="right">Media Type</StyledTableCell>
-              <StyledTableCell align="right">Genres</StyledTableCell>
-              <StyledTableCell align="right">ISBN</StyledTableCell>
-              <StyledTableCell align="right">Checked Out</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {search(list).map((list) => (
-              <StyledTableRow key={list.title}>
-                <StyledTableCell component="th" scope="row">
-                  {list.title}
-                </StyledTableCell>
-                <StyledTableCell align="right">{list.id}</StyledTableCell>
-                <StyledTableCell align="right">{list.id}</StyledTableCell>
-                <StyledTableCell align="right">{list.id}</StyledTableCell>
-                <StyledTableCell align="right">{list.id}</StyledTableCell>
-                <StyledTableCell align="right">{list.id}</StyledTableCell>
-              </StyledTableRow>
-            ))}
-            </TableBody>
-        </Table>
-      </div>
+      <MaterialTable
+      icons={{ 
+        Check: Check,
+        DetailPanel: ChevronRight,
+        Export: SaveAlt,
+        Filter: FilterList,
+        FirstPage: FirstPage,
+        LastPage: LastPage,
+        NextPage: ChevronRight,
+        PreviousPage: ChevronLeft,
+        Search: Search,
+        ThirdStateCheck: Remove,
+      }}
+      title=""
+      columns={[
+        { title: 'Title', field: 'title' },
+        { title: 'Author', field: 'author'},
+        { title: 'Genres', field: 'genres'},
+        {
+          title: 'Type',
+          field: 'type',
+          lookup: { 10: 'Movie', 20: 'Book', 30: 'Music' },
+        },
+        {
+          title: 'Checked Out',
+          field: 'checked',
+          lookup: { 10: 'IN', 20: 'OUT'},
+        },
+      ]}
+      data={[
+        { title: 'Mehmet', author: 'Baran', genres: 1987, type: 10 , checked: 10},
+        { title: 'Zerya Betül', author: 'Baran', genres: 2017, type: 20, checked: 20},
+      ]}        
+      options={{
+        filtering: true
+      }}
+    />
     </dev>
   );
 }
