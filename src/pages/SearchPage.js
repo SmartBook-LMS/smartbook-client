9759528355e0b59a5940a7941816c3727d7f0665
Daniel Kambich <dkambich@purdue.edu>
2020-11-21 15:35:54 -0500

    Create customizable browsing UI

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -4,7 +4,53 @@ import MUIDataTable from "mui-datatables";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { GetBookInfo } from "../core/requests";
 import { useConstructor } from "../core/constants";
+import {
+  Box,
+  Button,
+  Chip,
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 
+const mediaFields = {
+  Genre: {
+    operators: ["="],
+  },
+  Title: {
+    operators: ["="],
+  },
+};
+
+const bookFields = {
+  Author: {
+    operators: ["="],
+  },
+  ISBN: {
+    operators: ["="],
+  },
+};
+
+const movieFields = {
+  Director: {
+    operators: ["="],
+  },
+  Rating: {
+    operators: ["="],
+  },
+};
+
+const musicFields = {
+  Artist: {
+    operators: ["="],
+  },
+};
 function SearchPage() {
   const [data, setData] = useState([]);
 
@@ -66,12 +112,156 @@ function SearchPage() {
     },
   });
 
+  const onSearch = async () => {};
+
+  const [searchTags, setSearchTags] = useState([]);
+  const [searchField, setSearchField] = useState("genre");
+  const [searchText, setSearchText] = useState("");
+  const onSelectSearchField = ({ target: { value } }) => setSearchField(value);
+  const onChangeSearchText = ({ target: { value } }) => setSearchText(value);
+  const onAddSearchTag = () =>
+    setSearchTags([
+      ...searchTags,
+      { searchField: searchField, value: searchText },
+    ]);
+
+  const [mediaType, setMediaType] = useState("all");
+  const onSelectMediaType = ({ target: { value } }) => {
+    setMediaType(value);
+    setSearchField("genre");
+    setSearchText("");
+    setSearchTags([]);
+  };
   return (
     <div>
       <NavBar />
-      <MuiThemeProvider theme={myTheme}>
+      <Container maxWidth="md">
+        <Paper style={{ margin: 32, padding: 32 }}>
+          <Typography variant="h6">Search Catalog</Typography>
+          <Box marginBottom={2} />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <FormControl variant="outlined" fullWidth>
+                <InputLabel>Media Type</InputLabel>
+                <Select
+                  id="mediaType"
+                  value={mediaType}
+                  onChange={onSelectMediaType}
+                  label="Media Type"
+                >
+                  <MenuItem value="all">All</MenuItem>
+                  <MenuItem value="book">Book</MenuItem>
+                  <MenuItem value="movie">Movie</MenuItem>
+                  <MenuItem value="music">Music</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Box display="flex" flexDirection="row" alignItems="center">
+                <Box flex={1}>
+                  <FormControl variant="outlined" fullWidth>
+                    <InputLabel>Search Field</InputLabel>
+                    <Select
+                      id="searchField"
+                      value={searchField}
+                      onChange={onSelectSearchField}
+                      label="Search Field"
+                      fullWidth
+                    >
+                      {Object.keys(mediaFields).map((key, index) => (
+                        <MenuItem key={key} value={key.toLowerCase()}>
+                          {key}
+                        </MenuItem>
+                      ))}
+                      {mediaType === "book" &&
+                        Object.keys(bookFields).map((key, index) => (
+                          <MenuItem key={key} value={key.toLowerCase()}>
+                            {key}
+                          </MenuItem>
+                        ))}
+                      {mediaType === "movie" &&
+                        Object.keys(movieFields).map((key, index) => (
+                          <MenuItem key={key} value={key.toLowerCase()}>
+                            {key}
+                          </MenuItem>
+                        ))}
+                      {mediaType === "music" &&
+                        Object.keys(musicFields).map((key, index) => (
+                          <MenuItem key={key} value={key.toLowerCase()}>
+                            {key}
+                          </MenuItem>
+                        ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Box flex={3}>
+                  <TextField
+                    variant="outlined"
+                    onChange={onChangeSearchText}
+                    fullWidth
+                    placeholder="Field value..."
+                    value={searchText}
+                  />
+                </Box>
+                <Box flex={1}>
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    size="large"
+                    onClick={onAddSearchTag}
+                  >
+                    Add
+                  </Button>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid>
+              {searchTags.map((tag, index) => (
+                <Chip
+                  style={{ marginRight: 8, marginBottom: 8 }}
+                  key={index}
+                  label={`${tag.searchField} = ${tag.value}`}
+                  onDelete={() => {
+                    console.log(searchTags);
+                    searchTags.splice(index, 1);
+                    console.log(searchTags);
+                    setSearchTags([...searchTags]);
+                  }}
+                />
+              ))}
+            </Grid>
+            {/* <Grid item xs={6}>
+                <FormSelect
+                  id="genre"
+                  name="genre"
+                  label="Genre"
+                  control={control}
+                  defaultValue={genres[selectMediaType][0]}
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                >
+                  {genres[selectMediaType].map((genre, index) => (
+                    <MenuItem key={index} value={genre}>
+                      {genre}
+                    </MenuItem>
+                  ))}
+                </FormSelect>
+              </Grid> */}
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={onSearch}
+            >
+              Search Media
+            </Button>
+          </Grid>
+        </Paper>
+      </Container>
+      {/* <MuiThemeProvider theme={myTheme}>
         <MUIDataTable data={data} columns={columns} options={options} />
-      </MuiThemeProvider>
+      </MuiThemeProvider> */}
     </div>
   );
 }
