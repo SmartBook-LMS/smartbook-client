19c3c94c30cf75406df8e63f01c4a9a7c0a253fb
Daniel Kambich <dkambich@purdue.edu>
2020-11-21 19:57:13 -0500

    Create more robust UI

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -15,123 +15,68 @@ import {
   MenuItem,
   Paper,
   Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   TextField,
   Typography,
 } from "@material-ui/core";
 
-const mediaFields = {
-  Genre: {
-    operators: ["="],
-  },
-  Title: {
-    operators: ["="],
-  },
-};
+const mediaFields = ["Title", "Genre", "Description"];
 
-const bookFields = {
-  Author: {
-    operators: ["="],
-  },
-  ISBN: {
-    operators: ["="],
-  },
-};
+const bookFields = ["Author", "ISBN"];
 
-const movieFields = {
-  Director: {
-    operators: ["="],
-  },
-  Rating: {
-    operators: ["="],
-  },
-};
+const movieFields = ["Director", "Rating"];
 
-const musicFields = {
-  Artist: {
-    operators: ["="],
-  },
-};
+const musicFields = ["Artist"];
+
+const data = [
+  { title: "Test", genre: " Test", description: "Test" },
+  { title: "Test2", genre: " Test2", description: "Test2" },
+  { title: "Test3", genre: " Test3", description: "Test3" },
+];
+
 function SearchPage() {
-  const [data, setData] = useState([]);
+  const [searchResults, setSearchResults] = useState(data);
 
   useConstructor(async () => {
-    const bookData = await GetBookInfo();
-    setData(bookData);
-  });
-
-  const columns = [
-    {
-      name: "Media.Title",
-      label: "Title",
-      options: {
-        filter: true,
-        sort: true,
-      },
-    },
-    {
-      name: "Author",
-      label: "Author",
-      options: {
-        filter: true,
-        sort: false,
-      },
-    },
-    {
-      name: "Media.Genre",
-      label: "Genre",
-      options: {
-        filter: true,
-        sort: false,
-      },
-    },
-    {
-      name: "ISBN",
-      label: "ISBN",
-      options: {
-        filter: true,
-        sort: false,
-      },
-    },
-  ];
-
-  const options = {
-    filter: true,
-    filterType: "dropdown",
-    responsive: "vertical",
-    selectableRows: false,
-    enableNestedDataAccess: ".", // allows nested data separated by "." (see column names and the data structure above)
-  };
-
-  const myTheme = createMuiTheme({
-    overrides: {
-      MUIDataTable: {
-        responsiveScroll: {
-          maxHeight: "580px",
-        },
-      },
-    },
+    // const bookData = await GetBookInfo();
   });
 
   const onSearch = async () => {};
 
   const [searchTags, setSearchTags] = useState([]);
-  const [searchField, setSearchField] = useState("genre");
+  const [searchField, setSearchField] = useState("Title");
   const [searchText, setSearchText] = useState("");
   const onSelectSearchField = ({ target: { value } }) => setSearchField(value);
   const onChangeSearchText = ({ target: { value } }) => setSearchText(value);
-  const onAddSearchTag = () =>
-    setSearchTags([
-      ...searchTags,
-      { searchField: searchField, value: searchText },
-    ]);
+  const onAddSearchTag = () => {
+    if (searchText === "") return;
+    if (
+      searchTags.some(
+        (tag) => tag.searchField === searchField && searchText === tag.value
+      )
+    )
+      return;
+    setSearchTags([...searchTags, { searchField, value: searchText }]);
+  };
 
   const [mediaType, setMediaType] = useState("all");
   const onSelectMediaType = ({ target: { value } }) => {
     setMediaType(value);
-    setSearchField("genre");
+    setSearchField("Title");
     setSearchText("");
     setSearchTags([]);
   };
+
+  let fields = mediaFields;
+  if (mediaType === "book") fields = [...fields, ...bookFields];
+  if (mediaType === "movie") fields = [...fields, ...movieFields];
+  if (mediaType === "music") fields = [...fields, ...musicFields];
+
   return (
     <div>
       <NavBar />
@@ -168,33 +113,15 @@ function SearchPage() {
                       label="Search Field"
                       fullWidth
                     >
-                      {Object.keys(mediaFields).map((key, index) => (
-                        <MenuItem key={key} value={key.toLowerCase()}>
-                          {key}
+                      {fields.map((field) => (
+                        <MenuItem key={field} value={field}>
+                          {field}
                         </MenuItem>
                       ))}
-                      {mediaType === "book" &&
-                        Object.keys(bookFields).map((key, index) => (
-                          <MenuItem key={key} value={key.toLowerCase()}>
-                            {key}
-                          </MenuItem>
-                        ))}
-                      {mediaType === "movie" &&
-                        Object.keys(movieFields).map((key, index) => (
-                          <MenuItem key={key} value={key.toLowerCase()}>
-                            {key}
-                          </MenuItem>
-                        ))}
-                      {mediaType === "music" &&
-                        Object.keys(musicFields).map((key, index) => (
-                          <MenuItem key={key} value={key.toLowerCase()}>
-                            {key}
-                          </MenuItem>
-                        ))}
                     </Select>
                   </FormControl>
                 </Box>
-                <Box flex={3}>
+                <Box flex={3} padding={1}>
                   <TextField
                     variant="outlined"
                     onChange={onChangeSearchText}
@@ -203,7 +130,7 @@ function SearchPage() {
                     value={searchText}
                   />
                 </Box>
-                <Box flex={1}>
+                <Box flex={1} padding={1}>
                   <Button
                     fullWidth
                     variant="contained"
@@ -220,34 +147,14 @@ function SearchPage() {
                 <Chip
                   style={{ marginRight: 8, marginBottom: 8 }}
                   key={index}
-                  label={`${tag.searchField} = ${tag.value}`}
+                  label={`${tag.searchField} = "${tag.value}"`}
                   onDelete={() => {
-                    console.log(searchTags);
                     searchTags.splice(index, 1);
-                    console.log(searchTags);
                     setSearchTags([...searchTags]);
                   }}
                 />
               ))}
             </Grid>
-            {/* <Grid item xs={6}>
-                <FormSelect
-                  id="genre"
-                  name="genre"
-                  label="Genre"
-                  control={control}
-                  defaultValue={genres[selectMediaType][0]}
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                >
-                  {genres[selectMediaType].map((genre, index) => (
-                    <MenuItem key={index} value={genre}>
-                      {genre}
-                    </MenuItem>
-                  ))}
-                </FormSelect>
-              </Grid> */}
             <Button
               fullWidth
               variant="contained"
@@ -258,6 +165,32 @@ function SearchPage() {
             </Button>
           </Grid>
         </Paper>
+      </Container>
+      <Container maxWidth="md">
+        <TableContainer component={Paper} style={{ margin: 32, padding: 32 }}>
+          <Typography variant="h6">Search Results</Typography>
+
+          <Table>
+            <TableHead>
+              <TableRow>
+                {fields.map((field) => (
+                  <TableCell key={field}>{field}</TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {searchResults.map((row) => (
+                <TableRow key={row.name}>
+                  {fields.map((field) => (
+                    <TableCell scope="row">
+                      {row[field.toLowerCase()]}{" "}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Container>
       {/* <MuiThemeProvider theme={myTheme}>
         <MUIDataTable data={data} columns={columns} options={options} />
